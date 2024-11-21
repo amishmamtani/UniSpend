feefa201bc1518129494055146d9d3e33016ae91
Olive Wachira <o.wachira@mail.utoronto.ca>
2024-11-21 12:24:40 -0500

    Main class, opens budget tracker

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,9 +3,15 @@ package app;
 import interface_adapter.budget.BudgetController;
 import interface_adapter.budget.BudgetPresenter;
 import interface_adapter.budget.BudgetViewModel;
+import interface_adapter.budgettracker.BudgetTrackerController;
+import interface_adapter.budgettracker.BudgetTrackerPresenter;
+import interface_adapter.budgettracker.BudgetTrackerViewModel;
 import use_case.budget.BudgetInteractor;
 import use_case.budget.BudgetOutputBoundary;
+import use_case.budgettracker.BudgetTrackerInteractor;
+import use_case.budgettracker.BudgetTrackerOutputBoundary;
 import view.BudgetMakerView;
+import view.BudgetTrackerView;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,5 +20,11 @@ public class Main {
         BudgetInteractor budgetInteractor = new BudgetInteractor(BOB);
         BudgetController BPC = new BudgetController(budgetInteractor);
         new BudgetMakerView(BVM, BPC);
+
+        BudgetTrackerViewModel BVM2 = new BudgetTrackerViewModel();
+        BudgetTrackerOutputBoundary BTOB = new BudgetTrackerPresenter(BVM2);
+        BudgetTrackerInteractor budgetTrackerInteractor = new BudgetTrackerInteractor(BTOB);
+        BudgetTrackerController BTC = new BudgetTrackerController(budgetTrackerInteractor);
+        new BudgetTrackerView(BVM2, BTC);
     }
 }
