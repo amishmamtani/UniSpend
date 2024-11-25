c59aa1ec0159cca2b75cfcc4006f28342ef638a1
thiyaan <thiyaan.karunanayake@mail.utoronto.ca>
2024-11-24 19:04:02 -0500

    Completed BudgetCompare Contoller, Interactor, State, InputData, OutputData
    Updated BudgetTracker State, OutputData, InputData by getting rid of redundant imports

--- src/main/java/interface_adapter/budgetcompare/BudgetCompareState.java
+++ src/main/java/interface_adapter/budgetcompare/BudgetCompareState.java
@@ -1,4 +1,30 @@
 package interface_adapter.budgetcompare;
 
+import java.util.HashMap;
+
 public class BudgetCompareState {
+    private HashMap<String, Double> advisedAllocations;
+    private HashMap<String, Double> spentAllocations;
+    private HashMap<String, Double> netAllocations;
+    private double unspent_income;
+
+    public BudgetCompareState(HashMap<String, Double> advisedAllocations, HashMap<String, Double> spentAllocations,
+                                   HashMap<String, Double> netAllocations, double unspent_income){
+        this.advisedAllocations = advisedAllocations;
+        this.spentAllocations = spentAllocations;
+        this.netAllocations = netAllocations;
+        this.unspent_income = unspent_income;
+    }
+
+    public BudgetCompareState(){
+
+    }
+
+    public HashMap<String, Double> getAdvisedAllocations() {return advisedAllocations;}
+
+    public HashMap<String, Double> getSpentAllocations() {return spentAllocations;}
+
+    public HashMap<String, Double> getNetAllocations() {return netAllocations;}
+
+    public double getUnspent_income() {return unspent_income;}
 }
