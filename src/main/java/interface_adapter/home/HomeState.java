b84d61437c00dc7ecd7a3ff5020bc7edcb0d283d
Olive Wachira <o.wachira@mail.utoronto.ca>
2024-11-30 17:58:09 -0500

    edited budgetcompareview to only display matching categories, implemented the login interactor

--- src/main/java/interface_adapter/home/HomeState.java
+++ src/main/java/interface_adapter/home/HomeState.java
@@ -1,4 +1,13 @@
 package interface_adapter.home;
 
 public class HomeState {
+    private String emailId;
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
--- src/main/java/use_case/login/LogInInteractor.java
+++ src/main/java/use_case/login/LogInInteractor.java
@@ -1,12 +1,35 @@
 package use_case.login;
 
+import entity.User;
+import interface_adapter.user.MongoUserRepository;
+
 public class LogInInteractor implements LogInInputBoundary{
     private LogInOutputBoundary logInOutputBoundary;
+    private MongoUserRepository userRepo = new MongoUserRepository();
+    private User user;
     public LogInInteractor(LogInOutputBoundary logInOutputBoundary) {
+
         this.logInOutputBoundary = logInOutputBoundary;
     }
     @Override
     public void execute(LogInInputData loginInputData) {
-        logInOutputBoundary.prepareSuccessView(new LogInOutputData("", false));
+        final String emailID = loginInputData.getUsername();
+        final String password = loginInputData.getPassword();
+        user = userRepo.getUserByEmail(emailID);
+        if (user == null) {
+            logInOutputBoundary.prepareFailView("Account does not exist");
+            System.out.println("Account does not exist");
+        }
+        else {
+            final String pass = user.getPassword();
+            if (!pass.equals(password)) {
+                logInOutputBoundary.prepareFailView("Incorrect password");
+                System.out.println("Incorrect password");
+            }
+            else {
+                // set current user
+                logInOutputBoundary.prepareSuccessView(new LogInOutputData("", false));
+            }
+        }
     }
 }
