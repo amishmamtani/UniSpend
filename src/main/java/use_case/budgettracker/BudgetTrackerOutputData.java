1f116ebf1af82642e6cd2b499cd19b35b2093420
thiyaan <thiyaan.karunanayake@mail.utoronto.ca>
2024-11-30 20:32:40 -0500

    Wrote JavaDoc for all BudgetTrackerUseCase files

--- src/main/java/use_case/budgettracker/BudgetTrackerOutputData.java
+++ src/main/java/use_case/budgettracker/BudgetTrackerOutputData.java
@@ -2,12 +2,28 @@ package use_case.budgettracker;
 
 import java.util.HashMap;
 
+/**
+ * Encapsulates the output data for a budget tracker
+ */
 public class BudgetTrackerOutputData {
+    /** The user's total income */
     private final double income;
+    /** The categories and the amounts already spent by the user */
     private final HashMap<String, Double> alreadySpentCategories;
+    /** The amount of income left unspent */
     private final double unspent_income;
+    /** true if the user has spent more than their income */
     private final boolean spent_more_than_income;
 
+
+    /**
+     * Constructs a BudgetTrackerOutputData object
+     *
+     * @param income                User's total income.
+     * @param alreadySpentCategories Categories and the amounts already spent by the user.
+     * @param unspent_income        The amount of income left unspent.
+     * @param spent_more_than_income Indicates whether the user has overspent.
+     */
     public BudgetTrackerOutputData( double income, HashMap<String, Double> alreadySpentCategories,
                                     double unspent_income,
                                     boolean spent_more_than_income) {
@@ -17,11 +33,15 @@ public class BudgetTrackerOutputData {
         this.spent_more_than_income = spent_more_than_income;
     }
 
+    /** @return the user's total income */
     public double getIncome() {return income;}
 
+    /** @return the categories and the amounts already spent by the user */
     public HashMap<String, Double> getAlreadySpentCategories() {return alreadySpentCategories;}
 
+    /** @return the amount of income left unspent */
     public double getUnspent_income() {return unspent_income;}
 
+    /** @return whether the user has spent more than their income */
     public boolean isSpent_more_than_income() {return spent_more_than_income;}
 }
